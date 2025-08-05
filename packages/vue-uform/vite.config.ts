df823322dcd52e54bc973e6b0b5756ead33620be
tu6ge <tu6ge@qq.com>
2025-08-05 20:37:24 +0800

    chore: buiding support d.ts file

--- packages/vue-uform/vite.config.ts
+++ packages/vue-uform/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
