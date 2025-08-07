e02d0c503e1169182ab63eec38b0ece2860758a9
tu6ge <tu6ge@qq.com>
2025-08-07 22:14:50 +0800

    test(plugin): compile test

--- packages/vite-plugin/tests/main.test.ts
+++ packages/vite-plugin/tests/main.test.ts
@@ -1,33 +1,25 @@
-import { parse, compileTemplate } from "@vue/compiler-sfc";
-import {
-  parse as parseDOM,
-  transform,
-  generate,
-  NodeTypes,
-  ElementNode,
-  AttributeNode,
-  transformElement, // 处理元素节点
-} from "@vue/compiler-dom";
-import { expect, test } from "vitest";
+import { compileTemplate } from "@vue/compiler-sfc";
+import { transformFmodel } from "../src/core/index";
+import { describe, expect, test } from "vitest";
 
-// test("first", () => {
-//   const { descriptor } = parse("<template><h1>hello</h1></template>");
-//   let templateAst = parseDOM(descriptor.template.content);
-//   //console.log(templateAst);
-//   transform(templateAst, {
-//     nodeTransforms: [
-//       transformElement,
-//       (node) => {
-//         if (node.type === NodeTypes.TEXT) {
-//           node.content = "world";
-//         }
-//       },
-//     ],
-//   });
-//   const { code: transformedTemplate } = compileTemplate({
-//     source: templateAst,
-//     filename: "a.vue",
-//     transformAssetUrls: false,
-//   });
-//   console.log(transformedTemplate);
-// });
+function compile(code: string) {
+  return compileTemplate({
+    source: code,
+    filename: "anonymous.vue",
+    id: "xxx",
+    compilerOptions: {
+      nodeTransforms: [transformFmodel({})],
+    },
+  });
+}
+
+describe("name", () => {
+  test("default name", () => {
+    const original = compile(
+      `<input :value="value" @input="($event)=>update($event.target.value)" />`
+    );
+    const sugar = compile(`<input f-model />`);
+    // expect(sugar.code).toBe(original.code);
+    // expect((sugar.ast as any).children[0].props[0]).matchSnapshot();
+  });
+});
