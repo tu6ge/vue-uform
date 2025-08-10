b199770730c8102e13508c5fc79bc5490dec9110
tu6ge <tu6ge@qq.com>
2025-08-10 17:06:34 +0800

    feat: add some build-in rules and messages

--- packages/vue-uform/src/message.ts
+++ packages/vue-uform/src/message.ts
@@ -3,15 +3,15 @@ import { FieldNode } from "./field";
 export type MessageArg = {
   node: FieldNode;
   label: string;
-  arg: string[];
+  args: string[];
 };
 export type Messages = { [name: string]: (arg: MessageArg) => string };
 
-const createMessageArg = (node: FieldNode, arg: string[]): MessageArg => {
+const createMessageArg = (node: FieldNode, args: string[]): MessageArg => {
   return {
     node,
     label: node.label,
-    arg,
+    args,
   };
 };
 
@@ -19,14 +19,57 @@ export const messages: Messages = {
   required: ({ label }) => {
     return `${label} is required`;
   },
-  max: ({ label, arg }) => {
-    return `${label} must be less than ${arg[0]}`;
-  },
+
   number: ({ label }) => {
     return `${label} must be a number`;
   },
   confirm: ({ label }) => {
     return `${label} is not match`;
+  },
+  accept: ({ label }) => {
+    return `Please accept the ${label}.`;
+  },
+  alpha: ({ label }) => {
+    return `${label} can only contain alphabetical characters.`;
+  },
+  alphanumeric: ({ label }) => {
+    return `${label} can only contain letters and numbers.`;
+  },
+  between: ({ label, args }) => {
+    return `${label} must be between ${args[0]} and ${args[1]}.`;
+  },
+  email: ({ label }) => {
+    return `${label} must be a email url`;
+  },
+  ends_with: ({ label, args }) => {
+    return `${label} doesn’t end with ${args.join(",")}.`;
+  },
+  is: ({ label }) => {
+    return `${label} is not an allowed value.`;
+  },
+  length: ({ label }) => {
+    return `${label}'s length is not match.`;
+  },
+  lowercase: ({ label }) => {
+    return `${label} can only contain lowercase letters.`;
+  },
+  max: ({ label, args }) => {
+    return `${label} must be less than ${args[0]}`;
+  },
+  min: ({ label, args }) => {
+    return `${label} must be greater than ${args[0]}`;
+  },
+  not: ({ label, node }) => {
+    return `“${node.value.value}” is not an allowed ${label}.`;
+  },
+  starts_with: ({ label, args }) => {
+    return `${label} doesn’t start with ${args.join(",")}.`;
+  },
+  uppercase: ({ label }) => {
+    return `${label} can only contain uppercase letters.`;
+  },
+  url: () => {
+    return `Please enter a valid URL.`;
   },
 };
 
--- packages/vue-uform/src/validation-buildin.ts
+++ packages/vue-uform/src/validation-buildin.ts
@@ -17,10 +17,7 @@ const validationList: ValidationList = {
   },
   number: {
     validator({ value }: FieldNode): string | boolean {
-      if (/^[0-9\.eE]+$/.test(value.value as string)) {
-        return true;
-      }
-      return false;
+      return !isNaN(value.value as number);
     },
   },
   confirm: {
@@ -31,6 +28,137 @@ const validationList: ValidationList = {
         return true;
       }
       return false;
+    },
+  },
+  accepted: {
+    validator({ value }: FieldNode): string | boolean {
+      return ["yes", "on", "1", 1, true, "true"].includes(
+        value.value as string
+      );
+    },
+  },
+  alpha: {
+    validator({ value }: FieldNode): string | boolean {
+      return /^\p{L}+$/u.test(String(value.value));
+    },
+  },
+  alphanumeric: {
+    validator({ value }: FieldNode): string | boolean {
+      return /^[0-9\p{L}]+$/u.test(String(value.value));
+    },
+  },
+  between: {
+    validator({ value }: FieldNode, from: any, to: any): boolean {
+      if (!isNaN(value.value as number) && !isNaN(from) && !isNaN(to)) {
+        const val = 1 * (value.value as number);
+        from = Number(from);
+        to = Number(to);
+        const [a, b] = from <= to ? [from, to] : [to, from];
+        return val >= 1 * a && val <= 1 * b;
+      }
+      return false;
+    },
+  },
+  email: {
+    validator({ value }: FieldNode): boolean {
+      const isEmail =
+        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      return isEmail.test(String(value.value));
+    },
+  },
+  ends_with: {
+    validator({ value }: FieldNode, ...stack: string[]): boolean {
+      if (typeof value.value === "string" && stack.length) {
+        return stack.some((item) => {
+          return (value.value as string).endsWith(item);
+        });
+      } else if (typeof value === "string" && stack.length === 0) {
+        return true;
+      }
+      return false;
+    },
+  },
+  is: {
+    validator({ value }: FieldNode, ...stack: any[]): boolean {
+      return stack.some((item) => {
+        return item == value;
+      });
+    },
+  },
+  length: {
+    validator({ value }: FieldNode, first = "0", second = "Infinity"): boolean {
+      const start = parseInt(first);
+      const end = second == "Infinity" ? Infinity : parseInt(second);
+      const min: number = start <= end ? start : end;
+      const max: number = end >= start ? end : start;
+      if (typeof value.value === "string") {
+        return (
+          (value.value as string).length >= min &&
+          (value.value as string).length <= max
+        );
+      } else if (Array.isArray(value.value)) {
+        return value.value.length >= min && value.value.length <= max;
+      } else if (value.value && typeof value.value === "object") {
+        const length = Object.keys(value.value).length;
+        return length >= min && length <= max;
+      }
+      return false;
+    },
+  },
+  lowercase: {
+    validator({ value }: FieldNode): boolean {
+      return /^\p{Ll}+$/u.test(String(value.value));
+    },
+  },
+  max: {
+    validator({ value }: FieldNode, maximum) {
+      if (Array.isArray(value.value)) {
+        return value.value.length <= Number(maximum);
+      }
+      return Number(value) <= Number(maximum);
+    },
+  },
+  min: {
+    validator({ value }: FieldNode, minimum) {
+      if (Array.isArray(value.value)) {
+        return value.value.length >= Number(minimum);
+      }
+      return Number(value) >= Number(minimum);
+    },
+  },
+  not: {
+    validator({ value }: FieldNode, ...stack): boolean {
+      return stack.every((item) => {
+        return item !== value.value;
+      });
+    },
+  },
+  starts_with: {
+    validator({ value }: FieldNode, ...stack: string[]): boolean {
+      if (typeof value.value === "string" && stack.length) {
+        return stack.some((item) => {
+          return (value.value as string).startsWith(item);
+        });
+      } else if (typeof value === "string" && stack.length === 0) {
+        return true;
+      }
+      return false;
+    },
+  },
+  uppercase: {
+    validator({ value }: FieldNode): boolean {
+      return /^\p{Lu}+$/u.test(String(value.value));
+    },
+  },
+  url: {
+    validator({ value }: FieldNode, ...stack): boolean {
+      try {
+        const protocols = stack.length ? stack : ["http:", "https:"];
+        const url = new URL(String(value.value));
+        return protocols.includes(url.protocol);
+      } catch {
+        return false;
+      }
     },
   },
 };
