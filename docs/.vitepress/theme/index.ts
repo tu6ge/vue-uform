71c831cda48b928c5525652a31ddb637f06326aa
tu6ge <tu6ge@qq.com>
2025-08-14 12:30:38 +0800

    doc: support stackblitz

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+import StackblitzEmbed from "./components/StackblitzEmbed.vue";
+import DefaultTheme from "vitepress/theme";
+
+import "./styles/main.css";
+
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx: any) {
+    ctx.app.component("StackblitzEmbed", StackblitzEmbed);
+  },
+};
--- docs/examples.md
+++ docs/examples.md
@@ -1,1 +1,5 @@
 # Examples
+
+soon
+
+<!-- <StackblitzEmbed project-id="vorms-field-array" /> -->
