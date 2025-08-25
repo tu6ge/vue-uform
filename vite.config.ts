cf7badc4a3bcefa72ac96d4c6f3663503b97b41f
tu6ge <tu6ge@qq.com>
2025-08-25 14:00:05 +0800

    chore: change coverage configure

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     include: ["packages/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+
     environment: "happy-dom",
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
       reportsDirectory: "./coverage",
+      include: ["packages/**/*.ts"],
     },
   },
 });
